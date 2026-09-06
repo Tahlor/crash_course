27174238ce1c94f6e3c5cb19cbb9dbe002127863
Taylor Archibald <36053181+Tahlor@users.noreply.github.com>
2026-09-06 07:53:39 +0200

    Cache Amazon course for offline use

---
+++ leetcode/amazon/sw.js
@@ -0,0 +1,5 @@
+const CACHE='amazon-algo-course-v2';
+const CORE=['./','./index.html','./style.css','./app.js','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(resp=>{if(resp.ok&&new URL(e.request.url).origin===location.origin){const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return resp}).catch(()=>caches.match('./index.html'))))});
